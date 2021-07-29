c2d33916cfd2a28ca260ae25dd5922e0c0abd835
Nathan Ruiz <nathanruiz@protonmail.com>
2021-07-29 11:20:15 +0000

    Add maintainers and tests

--- pkgs/development/python-modules/environmental-override/default.nix
+++ pkgs/development/python-modules/environmental-override/default.nix
@@ -12,9 +12,15 @@ buildPythonPackage rec {
     sha256 = "1vhd37i6f8xh6kd61yxc2ynzgcln7v2p7fyzjmhbkdnws6gwfs6s";
   };
 
+  # No tests have been written for this library.
+  doCheck = false;
+
+  pythonImportsCheck = [ "environmental_override" ];
+
   meta = {
     description = "Easily configure apps using simple environmental overrides";
     homepage = "https://github.com/coddingtonbear/environmental-override";
     license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ nathanruiz ];
   };
 }
